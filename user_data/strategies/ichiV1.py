13bb9293ba9f706d4a29b9f70576afd0ce64ed89
meir <contact@flamingo.dev>
2025-04-08 10:48:22 +0200

    update strategy

--- user_data/strategies/ichiV1.py
+++ user_data/strategies/ichiV1.py
@@ -1,6 +1,5 @@
-import logging
+# --- Do not remove these libs ---
 from freqtrade.strategy.interface import IStrategy
-from freqtrade.strategy import CategoricalParameter, DecimalParameter, IntParameter
 from pandas import DataFrame
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
@@ -11,68 +10,38 @@ import technical.indicators as ftt
 from functools import reduce
 from datetime import datetime, timedelta
 from freqtrade.strategy import merge_informative_pair
+import numpy as np
 from freqtrade.strategy import stoploss_from_open
-from typing import Dict, Optional, Union
-
-logger = logging.getLogger(__name__)
 
 
 class ichiV1(IStrategy):
 
-    INTERFACE_VERSION = 3
-
-    def version(self) -> str:
-        return "v0.0.001"
-
-    # Futures
-    custom_leverage = 1.0
-
     # NOTE: settings as of the 25th july 21
     # Buy hyperspace params:
-    # "buy_min_fan_magnitude_gain": 1.002 # NOTE: Good value (Win% ~70%), alot of trades
-    # "buy_min_fan_magnitude_gain": 1.008 # NOTE: Very save value (Win% ~90%), only the biggest moves 1.008,
-
-    buy_trend_above_senkou_level = IntParameter(1, 8, default=5, space="buy")
-    buy_trend_bullish_level = IntParameter(1, 8, default=6, space="buy")
-    buy_fan_magnitude_shift_value = IntParameter(1, 8, default=5, space="buy")
-    buy_min_fan_magnitude_gain = DecimalParameter(
-        0.980, 1.020, default=1.002, space="buy"
-    )
+    buy_params = {
+        "buy_trend_above_senkou_level": 1,
+        "buy_trend_bullish_level": 6,
+        "buy_fan_magnitude_shift_value": 3,
+        "buy_min_fan_magnitude_gain": 1.002,  # NOTE: Good value (Win% ~70%), alot of trades
+        # "buy_min_fan_magnitude_gain": 1.008 # NOTE: Very save value (Win% ~90%), only the biggest moves 1.008,
+    }
 
     # Sell hyperspace params:
     # NOTE: was 15m but kept bailing out in dryrun
-    sell_trend_indicator = CategoricalParameter(
-        [
-            "trend_close_5m",
-            "trend_close_15m",
-            "trend_close_30m",
-            "trend_close_1h",
-            "trend_close_2h",
-            "trend_close_4h",
-            "trend_close_6h",
-            "trend_close_8h",
-        ],
-        default="trend_close_15m",
-        space="sell",
-    )
-
-    # ROI table:
-    minimal_roi = {
-        "0": 0.061 * custom_leverage,
-        "3": 0.023 * custom_leverage,
-        "6": 0.008 * custom_leverage,
-        "19": 0 * custom_leverage,
+    sell_params = {
+        "sell_trend_indicator": "trend_close_2h",
     }
 
+    # ROI table:
+    minimal_roi = {"0": 0.059, "10": 0.037, "41": 0.012, "114": 0}
+
     # Stoploss:
-    stoploss = -0.217 * custom_leverage
+    stoploss = -0.275
 
     # Optimal timeframe for the strategy
-    timeframe = "1m"
+    timeframe = "5m"
 
-    startup_candle_count = 130
-    # startup_candle_count = 96
-    # process_only_new_candles = True
+    startup_candle_count = 96
     process_only_new_candles = False
 
     trailing_stop = False
@@ -80,12 +49,9 @@ class ichiV1(IStrategy):
     # trailing_stop_positive_offset = 0.025
     # trailing_only_offset_is_reached = True
 
-    use_exit_signal = True
-    exit_profit_only = False
-    ignore_roi_if_entry_signal = False
-
-    # calcola solo gli indicatori strettamente necessari se True
-    optimize = False
+    use_sell_signal = True
+    sell_profit_only = False
+    ignore_roi_if_buy_signal = False
 
     plot_config = {
         "main_plot": {
@@ -113,129 +79,31 @@ class ichiV1(IStrategy):
         },
     }
 
-    def bot_start(self, **kwargs) -> None:
-        """
-        Called only once after bot instantiation.
-        :param **kwargs: Ensure to keep this here so updates to this won't break your strategy.
-        """
-
-        self.optimize = self.config["runmode"].value in ("live", "dry_run")
-
-        self.custom_leverage = self.config["custom_leverage"]
-
-        logger.info(
-            f"--> runmode: {self.config['runmode'].value} | custom_leverage: {self.custom_leverage} "
-        )
-
-    def bot_loop_start(self, current_time: datetime, **kwargs) -> None:
-        """
-        Called at the start of the bot iteration (one loop).
-        Might be used to perform pair-independent tasks
-        (e.g. gather some remote resource for comparison)
-        :param current_time: datetime object, containing the current datetime
-        :param **kwargs: Ensure to keep this here so updates to this won't break your strategy.
-        """
-
-        # logger.info(" bot_loop_start ")
-
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        logger.info(f"START populate_indicators : {metadata['pair']}")
-
         heikinashi = qtpylib.heikinashi(dataframe)
-        dataframe_open = heikinashi["open"]
-        # dataframe_open = dataframe['open']
-        # dataframe_close = heikinashi['close']
-        dataframe_close = dataframe["close"]
-        # dataframe['hk_close'] = heikinashi['close']
-        # dataframe['hk_high'] = heikinashi['high']
-        # dataframe['hk_low'] = heikinashi['low']
+        dataframe["open"] = heikinashi["open"]
+        # dataframe['close'] = heikinashi['close']
+        dataframe["high"] = heikinashi["high"]
+        dataframe["low"] = heikinashi["low"]
 
-        moltiplicatore = 1  # dipende dal timeframe
+        dataframe["trend_close_5m"] = dataframe["close"]
+        dataframe["trend_close_15m"] = ta.EMA(dataframe["close"], timeperiod=3)
+        dataframe["trend_close_30m"] = ta.EMA(dataframe["close"], timeperiod=6)
+        dataframe["trend_close_1h"] = ta.EMA(dataframe["close"], timeperiod=12)
+        dataframe["trend_close_2h"] = ta.EMA(dataframe["close"], timeperiod=24)
+        dataframe["trend_close_4h"] = ta.EMA(dataframe["close"], timeperiod=48)
+        dataframe["trend_close_6h"] = ta.EMA(dataframe["close"], timeperiod=72)
+        dataframe["trend_close_8h"] = ta.EMA(dataframe["close"], timeperiod=96)
 
-        if moltiplicatore > 1:
-            dataframe["trend_close_5m"] = ta.EMA(
-                dataframe_close, timeperiod=1 * moltiplicatore
-            )
-        else:
-            dataframe["trend_close_5m"] = dataframe_close
-
-        if not self.optimize or (
-            self.buy_trend_above_senkou_level.value >= 2
-            or self.buy_trend_bullish_level.value >= 2
-        ):
-            dataframe["trend_close_15m"] = ta.EMA(
-                dataframe_close, timeperiod=3 * moltiplicatore
-            )
-        if not self.optimize or (
-            self.buy_trend_above_senkou_level.value >= 3
-            or self.buy_trend_bullish_level.value >= 3
-        ):
-            dataframe["trend_close_30m"] = ta.EMA(
-                dataframe_close, timeperiod=6 * moltiplicatore
-            )
-        # if not self.optimize or (self.buy_trend_above_senkou_level.value >= 4 or self.buy_trend_bullish_level.value >= 4):
-        dataframe["trend_close_1h"] = ta.EMA(
-            dataframe_close, timeperiod=12 * moltiplicatore
-        )
-        # if not self.optimize or (self.buy_trend_above_senkou_level.value >= 5 or self.buy_trend_bullish_level.value >= 5):
-        dataframe["trend_close_2h"] = ta.EMA(
-            dataframe_close, timeperiod=24 * moltiplicatore
-        )
-        if not self.optimize or (
-            self.buy_trend_above_senkou_level.value >= 6
-            or self.buy_trend_bullish_level.value >= 6
-        ):
-            dataframe["trend_close_4h"] = ta.EMA(
-                dataframe_close, timeperiod=48 * moltiplicatore
-            )
-        if not self.optimize or (
-            self.buy_trend_above_senkou_level.value >= 7
-            or self.buy_trend_bullish_level.value >= 7
-        ):
-            dataframe["trend_close_6h"] = ta.EMA(
-                dataframe_close, timeperiod=72 * moltiplicatore
-            )
-        # if not self.optimize or (self.buy_trend_above_senkou_level.value >= 8 or self.buy_trend_bullish_level.value >= 8):
-        dataframe["trend_close_8h"] = ta.EMA(
-            dataframe_close, timeperiod=96 * moltiplicatore
-        )
-
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 1):
-            if moltiplicatore > 1:
-                dataframe["trend_open_5m"] = ta.EMA(
-                    dataframe_open, timepriod=1 * moltiplicatore
-                )
-            else:
-                dataframe["trend_open_5m"] = dataframe_open
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 2):
-            dataframe["trend_open_15m"] = ta.EMA(
-                dataframe_open, timeperiod=3 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 3):
-            dataframe["trend_open_30m"] = ta.EMA(
-                dataframe_open, timeperiod=6 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 4):
-            dataframe["trend_open_1h"] = ta.EMA(
-                dataframe_open, timeperiod=12 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 5):
-            dataframe["trend_open_2h"] = ta.EMA(
-                dataframe_open, timeperiod=24 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 6):
-            dataframe["trend_open_4h"] = ta.EMA(
-                dataframe_open, timeperiod=48 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 7):
-            dataframe["trend_open_6h"] = ta.EMA(
-                dataframe_open, timeperiod=72 * moltiplicatore
-            )
-        if not self.optimize or (self.buy_trend_bullish_level.value >= 8):
-            dataframe["trend_open_8h"] = ta.EMA(
-                dataframe_open, timeperiod=96 * moltiplicatore
-            )
+        dataframe["trend_open_5m"] = dataframe["open"]
+        dataframe["trend_open_15m"] = ta.EMA(dataframe["open"], timeperiod=3)
+        dataframe["trend_open_30m"] = ta.EMA(dataframe["open"], timeperiod=6)
+        dataframe["trend_open_1h"] = ta.EMA(dataframe["open"], timeperiod=12)
+        dataframe["trend_open_2h"] = ta.EMA(dataframe["open"], timeperiod=24)
+        dataframe["trend_open_4h"] = ta.EMA(dataframe["open"], timeperiod=48)
+        dataframe["trend_open_6h"] = ta.EMA(dataframe["open"], timeperiod=72)
+        dataframe["trend_open_8h"] = ta.EMA(dataframe["open"], timeperiod=96)
 
         dataframe["fan_magnitude"] = (
             dataframe["trend_close_1h"] / dataframe["trend_close_8h"]
@@ -251,145 +119,115 @@ class ichiV1(IStrategy):
             laggin_span=120,
             displacement=30,
         )
-        # dataframe['chikou_span'] = ichimoku['chikou_span'] NON UTILIZZARE: ha bias
-
-        # dataframe['tenkan_sen'] = ichimoku['tenkan_sen']
-        # dataframe['kijun_sen'] = ichimoku['kijun_sen']
+        dataframe["chikou_span"] = ichimoku["chikou_span"]
+        dataframe["tenkan_sen"] = ichimoku["tenkan_sen"]
+        dataframe["kijun_sen"] = ichimoku["kijun_sen"]
         dataframe["senkou_a"] = ichimoku["senkou_span_a"]
         dataframe["senkou_b"] = ichimoku["senkou_span_b"]
-        # dataframe['leading_senkou_span_a'] = ichimoku['leading_senkou_span_a']
-        # dataframe['leading_senkou_span_b'] = ichimoku['leading_senkou_span_b']
-        # dataframe['cloud_green'] = ichimoku['cloud_green']
-        # dataframe['cloud_red'] = ichimoku['cloud_red']
+        dataframe["leading_senkou_span_a"] = ichimoku["leading_senkou_span_a"]
+        dataframe["leading_senkou_span_b"] = ichimoku["leading_senkou_span_b"]
+        dataframe["cloud_green"] = ichimoku["cloud_green"]
+        dataframe["cloud_red"] = ichimoku["cloud_red"]
 
-        # dataframe['atr'] = ta.ATR(dataframe)
-
-        logger.info(f"END   populate_indicators : {metadata['pair']}")
+        dataframe["atr"] = ta.ATR(dataframe)
 
         return dataframe
 
-    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
-        logger.info(f"START populate_entry_trend : {metadata['pair']}")
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         conditions = []
 
         # Trending market
-        if self.buy_trend_above_senkou_level.value >= 1:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 1:
             conditions.append(dataframe["trend_close_5m"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_5m"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 2:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 2:
             conditions.append(dataframe["trend_close_15m"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_15m"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 3:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 3:
             conditions.append(dataframe["trend_close_30m"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_30m"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 4:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 4:
             conditions.append(dataframe["trend_close_1h"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_1h"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 5:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 5:
             conditions.append(dataframe["trend_close_2h"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_2h"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 6:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 6:
             conditions.append(dataframe["trend_close_4h"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_4h"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 7:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 7:
             conditions.append(dataframe["trend_close_6h"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_6h"] > dataframe["senkou_b"])
 
-        if self.buy_trend_above_senkou_level.value >= 8:
+        if self.buy_params["buy_trend_above_senkou_level"] >= 8:
             conditions.append(dataframe["trend_close_8h"] > dataframe["senkou_a"])
             conditions.append(dataframe["trend_close_8h"] > dataframe["senkou_b"])
 
         # Trends bullish
-        if self.buy_trend_bullish_level.value >= 1:
+        if self.buy_params["buy_trend_bullish_level"] >= 1:
             conditions.append(dataframe["trend_close_5m"] > dataframe["trend_open_5m"])
 
-        if self.buy_trend_bullish_level.value >= 2:
+        if self.buy_params["buy_trend_bullish_level"] >= 2:
             conditions.append(
                 dataframe["trend_close_15m"] > dataframe["trend_open_15m"]
             )
 
-        if self.buy_trend_bullish_level.value >= 3:
+        if self.buy_params["buy_trend_bullish_level"] >= 3:
             conditions.append(
                 dataframe["trend_close_30m"] > dataframe["trend_open_30m"]
             )
 
-        if self.buy_trend_bullish_level.value >= 4:
+        if self.buy_params["buy_trend_bullish_level"] >= 4:
             conditions.append(dataframe["trend_close_1h"] > dataframe["trend_open_1h"])
 
-        if self.buy_trend_bullish_level.value >= 5:
+        if self.buy_params["buy_trend_bullish_level"] >= 5:
             conditions.append(dataframe["trend_close_2h"] > dataframe["trend_open_2h"])
 
-        if self.buy_trend_bullish_level.value >= 6:
+        if self.buy_params["buy_trend_bullish_level"] >= 6:
             conditions.append(dataframe["trend_close_4h"] > dataframe["trend_open_4h"])
 
-        if self.buy_trend_bullish_level.value >= 7:
+        if self.buy_params["buy_trend_bullish_level"] >= 7:
             conditions.append(dataframe["trend_close_6h"] > dataframe["trend_open_6h"])
 
-        if self.buy_trend_bullish_level.value >= 8:
+        if self.buy_params["buy_trend_bullish_level"] >= 8:
             conditions.append(dataframe["trend_close_8h"] > dataframe["trend_open_8h"])
 
         # Trends magnitude
         conditions.append(
-            dataframe["fan_magnitude_gain"] >= self.buy_min_fan_magnitude_gain.value
+            dataframe["fan_magnitude_gain"]
+            >= self.buy_params["buy_min_fan_magnitude_gain"]
         )
         conditions.append(dataframe["fan_magnitude"] > 1)
 
-        for x in range(self.buy_fan_magnitude_shift_value.value):
+        for x in range(self.buy_params["buy_fan_magnitude_shift_value"]):
             conditions.append(
                 dataframe["fan_magnitude"].shift(x + 1) < dataframe["fan_magnitude"]
             )
 
         if conditions:
-            dataframe.loc[reduce(lambda x, y: x & y, conditions), "enter_long"] = 1
-
-        logger.info(f"END   populate_entry_trend : {metadata['pair']}")
+            dataframe.loc[reduce(lambda x, y: x & y, conditions), "buy"] = 1
 
         return dataframe
 
-    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         conditions = []
 
         conditions.append(
             qtpylib.crossed_below(
-                dataframe["trend_close_5m"], dataframe[self.sell_trend_indicator.value]
+                dataframe["trend_close_5m"],
+                dataframe[self.sell_params["sell_trend_indicator"]],
             )
         )
 
         if conditions:
-            dataframe.loc[reduce(lambda x, y: x & y, conditions), "exit_long"] = 1
+            dataframe.loc[reduce(lambda x, y: x & y, conditions), "sell"] = 1
 
         return dataframe
-
-    def leverage(
-        self,
-        pair: str,
-        current_time: datetime,
-        current_rate: float,
-        proposed_leverage: float,
-        max_leverage: float,
-        entry_tag: Optional[str],
-        side: str,
-        **kwargs,
-    ) -> float:
-        """
-        Customize leverage for each new trade. This method is only called in futures mode.
-
-        :param pair: Pair that's currently analyzed
-        :param current_time: datetime object, containing the current datetime
-        :param current_rate: Rate, calculated based on pricing settings in exit_pricing.
-        :param proposed_leverage: A leverage proposed by the bot.
-        :param max_leverage: Max leverage allowed on this pair
-        :param entry_tag: Optional entry_tag (buy_tag) if provided with the buy signal.
-        :param side: 'long' or 'short' - indicating the direction of the proposed trade
-        :return: A leverage amount, which is between 1.0 and max_leverage.
-        """
-        return self.custom_leverage
